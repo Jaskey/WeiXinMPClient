9e45dc30eec33ccad6e99450f549cdbd9424cfa9
Jaskey <linjunjie1103@gmail.com>
2014-08-11 18:51:52 +0800

    client commit

---
+++ src/client/RequestTextMessage.java
@@ -0,0 +1,16 @@
+package client;
+
+public class RequestTextMessage extends RequestMessage {
+    /** 
+     * �ظ�����Ϣ���� 
+     */  
+    private String Content;  
+  
+    public String getContent() {  
+        return Content;  
+    }  
+  
+    public void setContent(String content) {  
+        Content = content;  
+    }  
+}
---
+++ src/client/XMLRequest.java
@@ -0,0 +1,116 @@
+package client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+import javax.swing.JTextArea;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.core.util.QuickWriter;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import com.thoughtworks.xstream.io.xml.PrettyPrintWriter;
+import com.thoughtworks.xstream.io.xml.XppDriver;
+
+
+
+public class XMLRequest {
+
+
+	
+	
+	// //////////////////////////////////////////////////////////////////////
+	// Post message to URL
+	// //////////////////////////////////////////////////////////////////////
+	public static void postMessage(String xmlstr, String address, String subAddress,JTextArea chatBox) throws DocumentException, IOException
+			 {
+		System.out.println("post address: " + address);
+			
+			//xmlstr = URLEncoder.encode(xmlstr, "utf-8");  
+			
+
+			URL url = new URL(address);
+			URLConnection uc = url.openConnection();
+			HttpURLConnection conn = (HttpURLConnection) uc;
+			conn.setDoInput(true);
+			conn.setDoOutput(true);
+			conn.setRequestProperty("Accept-Charset", "utf-8");  
+			//conn.setRequestProperty("contentType", "utf-8"); 
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-type", "text/xml");
+			System.out.println("before POST:\n"+xmlstr);
+			PrintWriter pw = new PrintWriter(conn.getOutputStream());
+			pw.write(xmlstr);
+			pw.close();
+			System.out.println("conn.getInputStream() =" + conn.getInputStream());
+			InputStream is=conn.getInputStream();
+			
+			//BufferedReader br=new BufferedReader(new InputStreamReader(is, "UTF-8"));
+			//String line;
+/*			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+				chatBox.append(line+"\n");
+			}*/
+			chatBox.append("system replys:\n"+parseXmlContent(is)+"\n");
+			is.close();
+			//br.close();
+		 
+	}
+	
+	public static String testXMLStr(){
+		 RequestTextMessage msg=new RequestTextMessage();
+		 msg.setFromUserName("from user");
+		 msg.setToUserName("to user");
+		 msg.setCreateTime(1348831860);
+		 msg.setMsgId(1234567890123456l);
+		 msg.setMsgType("text");
+		 msg.setMsgType("1");
+		 return textMessageToXml(msg);
+	}	
+	
+	public static String textMessageToXml(RequestTextMessage textMsg){
+		xstream.alias("xml", textMsg.getClass());
+		return xstream.toXML(textMsg);
+	}
+	   private static XStream xstream = new XStream(new XppDriver() {
+		 	
+	    	@Override
+	        public HierarchicalStreamWriter createWriter(Writer out) {  
+	            return new PrettyPrintWriter(out) {  
+	                // 增加CDATA块标识  
+	                boolean cdata = true;  
+	                
+	                protected void writeText(QuickWriter writer, String text) {  
+	                    if (cdata) {  
+	                        writer.write("<![CDATA[");  
+	                        writer.write(text);  
+	                        writer.write("]]>");  
+	                    } else {  
+	                        writer.write(text);  
+	                    }  
+	                }  
+	            };  
+	        }  
+	    });  
+	   
+		public  static String  parseXmlContent(InputStream is) throws IOException, DocumentException {
+			SAXReader reader=new SAXReader();
+			reader.setEncoding("UTF-8");
+			BufferedReader br=new BufferedReader(new InputStreamReader(is, "UTF-8"));
+			Document document=reader.read(br);	//TODO	
+			Element root =document.getRootElement();
+			return root.element("Content").getText();
+		}
+}
